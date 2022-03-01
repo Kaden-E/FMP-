433ac3b5c2c8582e3ce1653525aa894a6299cdcf
Kaden-E <95853329+Kaden-E@users.noreply.github.com>
2022-03-01 11:32:30 +0000

    Added portal functionality
    Can now teleport between levels

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Portal : Collidable
+{
+    protected override void OnCollide(Collider2D coll)
+    {
+        if (coll.name == "Player"){
+            //teleport the player
+            SceneManager.LoadScene(1);
+
+
+        }
+    }
+
+
+
+
+}
