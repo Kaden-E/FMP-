dfeb5e699c8880e8f162d40ea20ce6746042dd2a
Kaden-E <95853329+Kaden-E@users.noreply.github.com>
2022-03-01 10:53:31 +0000

    Added chest scripts

---
+++ Assets/Scripts/Chest.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : Collidable
+{
+    protected override void OnCollide(Collider2D coll)
+    {
+        Debug.Log("Grant gold");
+    }
+
+
+
+
+}
