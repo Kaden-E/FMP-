9c2ac25cf52c58e1ef2382674d41a778413399b5
Kaden-E <95853329+Kaden-E@users.noreply.github.com>
2022-03-03 13:10:45 +0000

    Added basic weapon script

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : Collidable
+{
+    // Damage struct
+
+    public int damagePoint = 1;
+    public float pushForce = 2.0f
+
+    //Upgrades
+    public int WeaponLvl = 0;
+    private SpriteRenderer spriteRenderer;
+
+    //swing
+    private float cooldown = 0.5f
+    private float lastSwing;
+
+
+    protected override void Start()
+    {
+        base.Start();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if (Input.GetKeyDown(KeyCode.Space)){
+            if (Time.time - lastSwing > cooldown){
+                lastSwing = Time.time;
+                Swing();
+            }
+        }
+    }
+
+    private void Swing(){
+        Debug.Log("Swing");
+    }
+
+
+
+
+
+
+}
+
