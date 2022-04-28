cd4213f1db43fbc3b9562a732135b036510c4a72
Kaden-E <95853329+Kaden-E@users.noreply.github.com>
2022-04-28 10:01:49 +0100

    Connecting rooms using corridors

--- Assets/Scripts/Procedual generation stuff/RoomFirstDungeonGen.cs
+++ Assets/Scripts/Procedual generation stuff/RoomFirstDungeonGen.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class RoomFirstDungeonGen : SImpleRandomWalkMapGen
 {
@@ -28,8 +29,82 @@ public class RoomFirstDungeonGen : SImpleRandomWalkMapGen
         HashSet<Vector2Int> floor = new HashSet<Vector2Int>();
         floor = CreateSimpleRooms(roomsList);
 
+        List<Vector2Int> roomCenters = new List<Vector2Int>();
+        foreach (var room in roomsList)
+        {
+            roomCenters.Add((Vector2Int)Vector3Int.RoundToInt(room.center));
+        }
+        HashSet<Vector2Int> corridors = ConnectRooms(roomCenters);
+        floor.UnionWith(corridors);
+
+
         tilemapVisualiser.PaintFloorTiles(floor);
         WallGen.CreateWalls(floor, tilemapVisualiser);
+    }
+
+    private HashSet<Vector2Int> ConnectRooms(List<Vector2Int> roomCenters)
+    {
+        HashSet<Vector2Int> corridors = new HashSet<Vector2Int>();
+        var currentRoomCentre = roomCenters[Random.Range(0, roomCenters.Count)];
+        roomCenters.Remove(currentRoomCentre);
+
+        while (roomCenters.Count > 0)
+        {
+            Vector2Int closest = FindClosesetPointTo(currentRoomCentre, roomCenters);
+            roomCenters.Remove(closest);
+            HashSet<Vector2Int> newCorridor = CreateCorridor(currentRoomCentre, closest);
+            currentRoomCentre = closest;
+            corridors.UnionWith(newCorridor);
+        }
+        return corridors;
+    }
+
+    private HashSet<Vector2Int> CreateCorridor(Vector2Int currentRoomCentre, Vector2Int Destination)
+    {
+        HashSet < Vector2Int > corridor = new HashSet<Vector2Int>();
+        var position = currentRoomCentre;
+        corridor.Add(position);
+        while (position.y != Destination.y)
+        {
+            if(Destination.y > position.y)
+            {
+                position += Vector2Int.up;
+            }
+            else if (Destination.y < position.y)
+            {
+                position += Vector2Int.down;
+            }
+            corridor.Add(position);
+        }
+        while (position.x != Destination.x)
+        {
+            if (Destination.x > position.x)
+            {
+                position += Vector2Int.right;
+            }
+            else if(Destination.x < position.x)
+            {
+                position += Vector2Int.left;
+            }
+            corridor.Add(position);
+        }
+        return corridor;
+    }
+
+    private Vector2Int FindClosesetPointTo(Vector2Int currentRoomCentre, List<Vector2Int> roomCenters)
+    {
+        Vector2Int closest = Vector2Int.zero;
+        float Distance = float.MaxValue;
+        foreach (var position in roomCenters)
+        {
+            float currentDistance = Vector2.Distance(position, currentRoomCentre);
+            if(currentDistance < Distance)
+            {
+                Distance = currentDistance;
+                closest = position;
+            }
+        }
+        return closest;
     }
 
     private HashSet<Vector2Int> CreateSimpleRooms(List<BoundsInt> roomsList)
